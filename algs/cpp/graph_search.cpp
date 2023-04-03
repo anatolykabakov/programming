bb68a864adf238ea6db5491d750b6aa3923b3fdb
Anatoly Kabakov <anatoly.kabakov@evocargo.com>
2023-04-03 20:25:12 +0300

    NEW: new algorithms added

---
+++ algs/cpp/graph_search.cpp
@@ -0,0 +1,142 @@
+// https://www.programiz.com/dsa/graph-bfs
+#include <iostream>
+#include <list>
+#include <queue>
+#include <vector>
+
+
+class Graph {
+public:
+  Graph(int num_vertices): root_{true}, root_id_{0} {
+    adj_list_.resize(num_vertices);
+  }
+
+  void add_edge(int src, int dest, int w=1) {
+    if (root_) {
+      root_id_ = src;
+      root_ = false;
+    }
+    adj_list_[src].push_back({dest, w});
+  }
+
+  std::size_t num_vertices() const  {
+    return adj_list_.size();
+  }
+
+  int root_id() const {
+    return root_id_;
+  }
+
+  std::vector<std::pair<int, int>> connected_vertices(int i) const {
+    return adj_list_[i];
+  }
+private:
+  std::vector<std::vector<std::pair<int, int>>> adj_list_;
+  
+  bool root_;
+  int root_id_;
+};
+
+bool breadth_first_search(const Graph &graph, int start, int stop) {
+  std::queue<int> not_visited;
+  
+  std::vector<bool> visited(graph.num_vertices(), false);
+
+  not_visited.push(start);
+
+  while (!not_visited.empty()) {
+    int current_v_id = not_visited.front();
+    not_visited.pop();
+
+
+    visited[current_v_id] = true;
+
+    std::cout << "current vertex id " << current_v_id << std::endl;
+    if (current_v_id == stop) {
+      return true;
+    }
+
+    for (const auto& [v_id, w] : graph.connected_vertices(current_v_id)) {
+      if (!visited[v_id]) {
+        not_visited.push(v_id);
+      }
+    }
+  }
+
+  return false;
+}
+
+bool dijkstra_search(const Graph& graph, int start, int stop) {
+  std::vector<int> costs(graph.num_vertices(), 1000000000);
+  std::vector<bool> visited(graph.num_vertices(), false);
+  std::queue<int> queue;
+
+  queue.push(start);
+  costs[start] = 0;
+
+  while (!queue.empty()) {
+    int node_id = queue.front();
+    queue.pop();
+    visited[node_id] = true;
+    int node_cost = costs[node_id];
+
+    if (node_id == stop) {
+      std::cout << "path from " << start << " to " << stop << " found with cost " << node_cost << std::endl;
+      return true;
+    }
+
+    for (const auto& [id, w] : graph.connected_vertices(node_id)) {
+      int new_cost = node_cost + w;
+      if (new_cost < costs[id]) {
+        costs[id] = new_cost;
+      }
+    }
+    
+    int min_cost_node_id = 0;
+    int lowest_cost = 1000000000;
+    bool lowest_cost_node_found = false;
+    for (int i = 0; i < costs.size(); ++i) {
+      int node_cost = costs[i];
+      if (node_cost < lowest_cost && !visited[i]) {
+        min_cost_node_id = i;
+        lowest_cost = node_cost;
+        lowest_cost_node_found = true;
+      }
+    }
+    if (lowest_cost_node_found) {
+      queue.push(min_cost_node_id);
+    }
+  }
+  return false;
+}
+
+void bfs_test() {
+  Graph graph(7);
+  graph.add_edge(0, 1);
+  graph.add_edge(0, 3);
+  graph.add_edge(0, 2);
+  graph.add_edge(2, 1);
+  graph.add_edge(2, 4);
+  std::cout << "find path from 0 to 4 " << std::endl; 
+  std::cout << breadth_first_search(graph, 0, 4) << std::endl;
+}
+
+void dijkstra_test() {
+  Graph g(7);
+  g.add_edge(0, 1, 2); // A->B 2
+  g.add_edge(0, 2, 1); // A->C 2
+  g.add_edge(1, 5, 7); // B->F 7
+  g.add_edge(2, 3, 5); // C->D 5
+  g.add_edge(2, 4, 2); // C->E 2
+  g.add_edge(3, 5, 2); // D->F 2
+  g.add_edge(4, 5, 1); // E->F 1
+  g.add_edge(5, 6, 1); // F->G 1
+  dijkstra_search(g, 0, 6);
+}
+
+int main() {
+  std::string names = "ABCDEFG";
+  // bfs_test();
+  dijkstra_test();
+  return 0;
+}
---
+++ algs/cpp/linked_list.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+struct Node {
+  Node* next;
+  int value;
+  Node(int x): value{x}, next{nullptr} {}
+};
+
+class LinkedList {
+public:
+  LinkedList(): size_(0), root_{nullptr} {}
+
+  void add(int value) {
+    if (root_ == nullptr) {
+      root_ = new Node(value);
+      size_ =+ 1;
+      return;
+    }
+
+    Node *node = root_;
+    while (node->next != nullptr) {
+      node = node->next;
+    }
+
+    node->next = new Node(value);
+    size_ += 1;
+  }
+
+  int size() {return size_; }
+
+  void print() {
+    Node *node = root_;
+    std::cout << node->value << std::endl;
+    while (node->next != nullptr) {
+      node = node->next;
+      std::cout << node->value << std::endl;
+    }
+  }
+
+private:
+  Node *root_;
+  int size_;
+};
+
+int main() {
+  LinkedList ll;
+  ll.add(0);
+  ll.add(1);
+  ll.add(2);
+  ll.add(3);
+  ll.print();
+
+  return 0;
+}
