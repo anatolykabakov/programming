36c81e07aede729040677703a466bf118b00159a
Anatoly Kabakov <anatoly.kabakov@evocargo.com>
2023-05-06 18:27:54 +0300

    UPD: examples of multithreading added

---
+++ cpp/multithreading/unique_lock.cpp
@@ -0,0 +1,108 @@
+#include <thread>
+#include <chrono>
+#include <iostream>
+#include <mutex>
+#include <list>
+#include <limits>
+
+class WarehouseEmpty {};
+
+unsigned short c_SpecialItem = std::numeric_limits<unsigned short>::max();
+
+class Warehouse {
+  std::list<unsigned short> m_Store;
+
+public:
+  void AcceptItem(unsigned short item) { m_Store.push_back(item); }
+  unsigned short HandLastItem() {
+    if (m_Store.empty())
+      throw WarehouseEmpty();
+    unsigned short item = m_Store.front();
+    if (item != c_SpecialItem)
+      m_Store.pop_front();
+    return item;
+  }
+};
+
+Warehouse g_FirstWarehouse;
+Warehouse g_SecondWarehouse;
+std::timed_mutex g_FirstMutex;
+std::mutex g_SecondMutex;
+
+int main() {
+  auto suplier = []() {
+    for (unsigned short i = 0, j = 0; i < 10 && j < 10;) {
+      if (i < 100 && g_FirstMutex.try_lock()) {
+        g_FirstWarehouse.AcceptItem(i);
+        i++;
+        g_FirstMutex.unlock();
+      }
+      if (j < 100 && g_SecondMutex.try_lock()) {
+        g_SecondWarehouse.AcceptItem(j);
+        j++;
+        g_SecondMutex.unlock();
+      }
+      std::this_thread::yield();
+    }
+    std::lock_guard<std::timed_mutex> first_guard(g_FirstMutex);
+    std::lock_guard<std::mutex> second_guard(g_SecondMutex);
+    // g_FirstMutex.lock();
+    // g_SecondMutex.lock();
+    g_FirstWarehouse.AcceptItem(c_SpecialItem);
+    g_SecondWarehouse.AcceptItem(c_SpecialItem);
+    // g_FirstMutex.unlock();
+    // g_SecondMutex.unlock();
+  };
+  auto consumer = []() {
+    while (true) {
+      // g_FirstMutex.lock();
+      unsigned short item = 0;
+      try {
+        std::lock_guard<std::timed_mutex> first_mutex(g_FirstMutex);
+        item = g_FirstWarehouse.HandLastItem();
+      } catch (Warehouse) {
+        std::cout << "Warehouse is empty!\n";
+      }
+      // g_FirstMutex.unlock();
+      if (item == c_SpecialItem)
+        break;
+      std::cout << "Got new item: " << item << "!\n";
+      std::this_thread::sleep_for(std::chrono::seconds(4));
+    }
+  };
+  auto impatientConsumer = []() {
+    while (true) {
+      unsigned short item = 0;
+      std::unique_lock<std::timed_mutex> firstGuard(g_FirstMutex, std::defer_lock);
+      if (firstGuard.try_lock_for(std::chrono::seconds(2))) {
+        try {
+          item = g_FirstWarehouse.HandLastItem();
+        } catch (const WarehouseEmpty&) {
+          std::cout << "Warehouse is empty! I'm mad!!!11\n";
+        }
+        firstGuard.unlock();
+      } else {
+        std::cout << "First warehouse always busy!!!\n";
+        try {
+          std::unique_lock<std::mutex> guard(g_SecondMutex);
+          item = g_SecondWarehouse.HandLastItem();
+        } catch (const WarehouseEmpty&) {
+          std::cout << "2nd warehouse is empty!!!!11\n";
+        }
+      }
+      if (item == c_SpecialItem)
+        break;
+      std::cout << "At last I got new item: " << item << "!\n";
+      std::this_thread::sleep_for(std::chrono::seconds(4));
+    }
+  };
+
+  std::thread supplierThread(suplier);
+  std::thread consumerThread(consumer);
+  std::thread impatientConsumerThread(impatientConsumer);
+
+  supplierThread.join();
+  consumerThread.join();
+  impatientConsumerThread.join();
+  return 0;
+}
