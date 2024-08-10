e6399ce8d667a9b7d7d7a01249ba720951be4801
anatoly kabakov <anatoly.kabakov@mail.ru>
2024-08-10 18:15:04 +0000

    example of breakpad added

--- cpp/exception_handler/src/exception_handler.cpp
+++ cpp/exception_handler/src/exception_handler.cpp
@@ -1,22 +1,12 @@
 #include <client/linux/handler/exception_handler.h>
 #include <client/linux/handler/minidump_descriptor.h>
+#include <iostream>
 
-bool callback(const google_breakpad::MinidumpDescriptor& descriptor, void* context, bool succeeded)
-{
-  std::cout << "dump callback called" << std::endl;
-  if (!succeeded) {
-    return false;
-  }
-
-  Framework* f = (Framework*)context;
-
-  ::rename(descriptor.path(), p->dumpPath().c_str());
-
-  return false;  // Return false allows other handlers to handle exception.
-}
+bool callback(const google_breakpad::MinidumpDescriptor& descriptor, void* context, bool succeeded);
 
 class Framework {
-  Framework(const std::string& dumpDir) : dumpDir_(dumpDir)
+public:
+  Framework(std::string dumpDir) : dumpDir_(dumpDir)
   {
     exception_handler = std::make_unique<google_breakpad::ExceptionHandler>(
         google_breakpad::MinidumpDescriptor(dumpDir),  // dump descriptor
@@ -29,7 +19,13 @@ class Framework {
     std::cout << "Exception handler registered" << std::endl;
   }
 
-  std::string dumpPath() { return dumpPath_ + "/minidump.dmp"; }
+  std::string dumpPath() { return dumpDir_ + "/minidump.dmp"; }
+
+  void crash()
+  {
+    volatile int* a = (int*)(NULL);
+    *a = 1;
+  }
 
   ~Framework() { std::cout << "Exception handler destroyed" << std::endl; }
 
@@ -38,4 +34,22 @@ private:
   std::string dumpDir_;
 };
 
-int main() { Framework f; }
+bool callback(const google_breakpad::MinidumpDescriptor& descriptor, void* context, bool succeeded)
+{
+  std::cout << "dump callback called" << std::endl;
+  if (!succeeded) {
+    return false;
+  }
+
+  Framework* f = (Framework*)context;
+
+  ::rename(descriptor.path(), f->dumpPath().c_str());
+
+  return false;  // Return false allows other handlers to handle exception.
+}
+
+int main()
+{
+  Framework f(".");
+  f.crash();
+}
